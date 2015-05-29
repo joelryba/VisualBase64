1c226d5b873097bd019a91041c5b3591b07774da
joelryba <jtryba@msn.com>
2015-05-29 09:58:10 -0400

    commit to master

---
+++ VisualBase64App/Main.designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtBase64 = New System.Windows.Forms.TextBox()
+        Me.btnFileSave = New System.Windows.Forms.Button()
+        Me.btnFileOpen = New System.Windows.Forms.Button()
+        Me.btnPaste = New System.Windows.Forms.Button()
+        Me.btnCopy = New System.Windows.Forms.Button()
+        Me.btnFileExport = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtBase64
+        '
+        Me.txtBase64.Location = New System.Drawing.Point(46, 124)
+        Me.txtBase64.MaxLength = 80000000
+        Me.txtBase64.Multiline = True
+        Me.txtBase64.Name = "txtBase64"
+        Me.txtBase64.Size = New System.Drawing.Size(837, 372)
+        Me.txtBase64.TabIndex = 0
+        '
+        'btnFileSave
+        '
+        Me.btnFileSave.Location = New System.Drawing.Point(426, 524)
+        Me.btnFileSave.Name = "btnFileSave"
+        Me.btnFileSave.Size = New System.Drawing.Size(115, 88)
+        Me.btnFileSave.TabIndex = 1
+        Me.btnFileSave.Text = "Convert Base64 to Binary and Save"
+        Me.btnFileSave.UseVisualStyleBackColor = True
+        '
+        'btnFileOpen
+        '
+        Me.btnFileOpen.Location = New System.Drawing.Point(426, 27)
+        Me.btnFileOpen.Name = "btnFileOpen"
+        Me.btnFileOpen.Size = New System.Drawing.Size(115, 77)
+        Me.btnFileOpen.TabIndex = 2
+        Me.btnFileOpen.Text = "Open Binary File and Convert to Base64"
+        Me.btnFileOpen.UseVisualStyleBackColor = True
+        '
+        'btnPaste
+        '
+        Me.btnPaste.Location = New System.Drawing.Point(913, 154)
+        Me.btnPaste.Name = "btnPaste"
+        Me.btnPaste.Size = New System.Drawing.Size(101, 67)
+        Me.btnPaste.TabIndex = 3
+        Me.btnPaste.Text = "Paste Clipboard to Base64 box"
+        Me.btnPaste.UseVisualStyleBackColor = True
+        '
+        'btnCopy
+        '
+        Me.btnCopy.Location = New System.Drawing.Point(913, 281)
+        Me.btnCopy.Name = "btnCopy"
+        Me.btnCopy.Size = New System.Drawing.Size(101, 68)
+        Me.btnCopy.TabIndex = 4
+        Me.btnCopy.Text = "Copy Base64 Box to Clipboard"
+        Me.btnCopy.UseVisualStyleBackColor = True
+        '
+        'btnFileExport
+        '
+        Me.btnFileExport.Location = New System.Drawing.Point(913, 405)
+        Me.btnFileExport.Name = "btnFileExport"
+        Me.btnFileExport.Size = New System.Drawing.Size(101, 64)
+        Me.btnFileExport.TabIndex = 5
+        Me.btnFileExport.Text = "Export Base64 box to text file"
+        Me.btnFileExport.UseVisualStyleBackColor = True
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1073, 624)
+        Me.Controls.Add(Me.btnFileExport)
+        Me.Controls.Add(Me.btnCopy)
+        Me.Controls.Add(Me.btnPaste)
+        Me.Controls.Add(Me.btnFileOpen)
+        Me.Controls.Add(Me.btnFileSave)
+        Me.Controls.Add(Me.txtBase64)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.Name = "Main"
+        Me.Text = "Base64 Tool"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtBase64 As TextBox
+    Friend WithEvents btnFileSave As Button
+    Friend WithEvents btnFileOpen As Button
+    Friend WithEvents btnPaste As Button
+    Friend WithEvents btnCopy As Button
+    Friend WithEvents btnFileExport As Button
+End Class
---
+++ VisualBase64App/Main.vb
@@ -0,0 +1,126 @@
+﻿Public Class Main
+    Private Sub btnFileSave_Click(sender As Object, e As EventArgs) Handles btnFileSave.Click
+        Dim base64String As String
+        Dim SaveFileDialog1 As New SaveFileDialog()
+
+        'Write out the decoded data. 
+        Dim outFile As System.IO.FileStream
+
+        If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+            Me.Cursor = Cursors.WaitCursor
+            base64String = Me.txtBase64.Text
+
+            ' Convert the Base64 UUEncoded input into binary output. 
+            Dim binaryData() As Byte
+            Try
+                binaryData = System.Convert.FromBase64String(base64String)
+            Catch exp As System.ArgumentNullException
+                Me.Cursor = Cursors.Default
+                MsgBox("Base 64 string is null.")
+                Return
+            Catch exp As System.FormatException
+                Me.Cursor = Cursors.Default
+                MsgBox("Base 64 length is not even multiple of 4.")
+                Return
+            End Try
+
+            outFile = SaveFileDialog1.OpenFile()
+            If (outFile IsNot Nothing) Then
+                ' Code to write the stream goes here.
+                Try
+                    outFile.Write(binaryData, 0, binaryData.Length - 1)
+                    outFile.Close()
+                Catch exp As System.Exception
+                    ' Error creating stream or writing to it. Resignal message to user
+                    Me.Cursor = Cursors.Default
+                    MsgBox(exp.Message)
+                End Try
+                outFile.Close()
+            End If
+            Me.Cursor = Cursors.Default
+        End If
+
+    End Sub
+
+    Private Sub btnFileOpen_Click(sender As Object, e As EventArgs) Handles btnFileOpen.Click
+        Dim openFileDialog1 As New OpenFileDialog()
+
+        If openFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            ' open the file
+
+            Dim inFile As System.IO.FileStream
+            Dim binaryData() As Byte
+
+            Me.Cursor = Cursors.WaitCursor
+
+            Try
+                inFile = openFileDialog1.OpenFile()
+                ReDim binaryData(inFile.Length)
+                Dim bytesRead As Long = inFile.Read(binaryData,
+                                                0,
+                                                inFile.Length)
+                inFile.Close()
+            Catch exp As System.Exception
+                ' Error creating stream or reading from it.
+                Me.Cursor = Cursors.Default
+                MsgBox(exp.Message)
+                Return
+            End Try
+
+            ' Convert the binary input into Base64 UUEncoded output. 
+            Dim base64String As String
+            Try
+                base64String = System.Convert.ToBase64String(binaryData, 0, binaryData.Length)
+            Catch exp As System.ArgumentNullException
+                Me.Cursor = Cursors.Default
+                MsgBox("Binary data array is null.")
+                Return
+            End Try
+
+            ' Write the UUEncoded version to the output file. 
+            Me.txtBase64.Text = base64String
+            Me.Cursor = Cursors.Default
+
+        End If
+
+    End Sub
+
+    Private Sub btnPaste_Click(sender As Object, e As EventArgs) Handles btnPaste.Click
+        Me.txtBase64.Text = Clipboard.GetText
+
+    End Sub
+
+    Private Sub btnCopy_Click(sender As Object, e As EventArgs) Handles btnCopy.Click
+        Clipboard.Clear()
+        Clipboard.SetText(Me.txtBase64.Text)
+    End Sub
+
+    Private Sub btnFileExport_Click(sender As Object, e As EventArgs) Handles btnFileExport.Click
+        'Write out the decoded data. 
+        Dim SaveFileDialog1 As New SaveFileDialog()
+        Dim outFile As System.IO.FileStream
+        Dim fileWriter As System.IO.StreamWriter
+        Dim base64String As String
+
+        If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+            Me.Cursor = Cursors.WaitCursor
+            base64String = Me.txtBase64.Text
+            outFile = SaveFileDialog1.OpenFile()
+            If (outFile IsNot Nothing) Then
+                ' Code to write the stream goes here.
+                Try
+                    fileWriter = New System.IO.StreamWriter(outFile)
+                    fileWriter.WriteLine(base64String)
+                    outFile.Close()
+                Catch exp As System.Exception
+                    ' Error creating stream or writing to it. Resignal message to user
+                    Me.Cursor = Cursors.Default
+                    MsgBox(exp.Message)
+                End Try
+                outFile.Close()
+            End If
+            Me.Cursor = Cursors.Default
+        End If
+
+    End Sub
+End Class
